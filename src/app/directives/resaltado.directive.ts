55664d6c5f55aeb0c5986ce3627ef2e3e56622ec
Allan <vredes@live.com>
2020-07-18 00:48:36 -0400

    corregido: directiva personalizada

--- src/app/directives/resaltado.directive.ts
+++ src/app/directives/resaltado.directive.ts
@@ -1,14 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
 
-  constructor( private el:ElementRef ) {
-    
-    el.nativeElement.style.backgroundColor = "yellow"
+  @Input("appResaltado") nuevoColor: string;
 
+  constructor( private el:ElementRef ) {
    }
+
+
+   @HostListener('mouseenter') mouseEntro() {
+    this.resaltar( this.nuevoColor || 'red' );   
+  }
+
+   @HostListener('mouseleave') mouseSalio() {  
+    this.resaltar( null );   
+
+  }
+
+  private resaltar( color:string ){
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
